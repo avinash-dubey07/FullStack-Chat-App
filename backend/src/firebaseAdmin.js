876afdffa0b50f84e9188d66bf6857c09a76b381
avinash-dubey07 <avinash.dubey2700@gmail.com>
2025-02-06 01:09:39 +0530

    Deploying check

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -1,5 +1,13 @@
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: 'json' }; 
+// import serviceAccount from "./serviceAccountKey.json" assert { type: 'json' }; 
+// const serviceAccount = require("./serviceAccountKey.json");
+import fs from "fs";
+import path from "path";
+
+// Read the JSON file synchronously
+const serviceAccount = JSON.parse(
+  fs.readFileSync(path.resolve("src/serviceAccountKey.json"), "utf8")
+);
 
 
 admin.initializeApp({
